60202757c54bb59f26acbcfbca51441429347512
zesanahmed <zesanahmed593@gmail.com>
2024-06-22 02:54:03 +0600

    phones get method added

--- index.js
+++ index.js
@@ -23,27 +23,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const productDB = client.db("productDB");
-    const phoneCollection = database.collection("phoneCollection");
+    const phoneDB = client.db("phoneDB");
+    const phoneCollection = phoneDB.collection("phoneCollection");
+
+
 
     app.post("/phones",async (req,res) => {
-      const phonesData = req.body;
-      const result = await phoneCollection.insertOne(phoneCollection);
+      const phoneData = req.body;
+      const result = await phoneCollection.insertOne(phoneData);
+      res.send(result)
+    })
+
+    app.get("/phones",async (req,res) => {
+      const phoneData = phoneCollection.find();
+      const result = await phoneData.toArray();
       res.send(result)
     })
 
 
     console.log("Database is connected");
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.log);
 
 
 
 app.get('/',(req,res) => {
     res.send('Route is working')
+});
+app.get('/users',(req,res) => {
+    const user = {
+      name:"jisan",
+      id: 23,
+      wsl:"lsdkf"
+    }
+    res.send(user)
 });
 
 app.listen(port,(req,res) => {
